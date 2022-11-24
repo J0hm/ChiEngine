fa92969dc91ebeee637eadead5fdb62d5aa8ed2a
J0hm <johnkerr2003@gmail.com>
2022-11-23 22:50:23 -0500

    Removed Algorithms.cpp in favor of static header library implementation

--- src/Algorithms.h
+++ src/Algorithms.h
@@ -1,14 +1,45 @@
 #ifndef CHIENGINE_ALGORITHMS_H
 #define CHIENGINE_ALGORITHMS_H
 #include <vector>
+#include <iterator>
+#include <regex>
+#include "Types.h"
 
 class Algorithms {
 public:
-    static void split(std::vector<std::string> &list, std::string str, std::string d);
+    static void split(std::vector<std::string> &list, std::string str, std::string d){
+        // passing -1 as the submatch index parameter performs splitting
+        std::regex re(d);
+        std::sregex_token_iterator
+                first{str.begin(), str.end(), re, -1},
+                last;
+        list = {first, last};
+    };
 
-    static std::string bitBoardToString(int64 bb);
+    static std::string bitBoardToString(int64 bb) {
+        std::string res = "";
+        std::stringstream ss;
+        std::bitset<64>b(bb);
+        std::string tmp = b.to_string();
+        int count =0;
+        for (int i =0; i < 8; i ++)
+        {
+            res += "\n";
+            for (int j=0;j <8;j++)
+            {
+                res += tmp[count];
+                count ++;
+            }
+        }
 
-    static char asciiCharToLower(char in);
+        return res;
+    }
+
+    static char asciiCharToLower(char in) {
+        if (in <= 'Z' && in >= 'A')
+            return in - ('Z' - 'z');
+        return in;
+    }
 };
 
 #endif //CHIENGINE_ALGORITHMS_H
