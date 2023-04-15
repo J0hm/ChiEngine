cfb95a5ed83ad0a4d995a189ce950c8b42b8e8b0
J0hm <johnkerr2003@gmail.com>
2023-04-15 15:12:54 -0400

    Revert "Torch is now found but throws an error when loading the model... What? C10.dll????"
    This reverts commit c534875b198d7fa59b1e284d1af3edf2922a6b31.

--- Evaluate.cpp
+++ Evaluate.cpp
@@ -1,19 +1,17 @@
 #include "Evaluate.h"
-#include <torch/script.h>
+#include <torch/csrc/jit/serialization/import.h>
 
-
-Evaluator::Evaluator(const std::string &netPath) {
+Evaluator::Evaluator(const std::string& netPath) {
     try {
         module = torch::jit::load(netPath);
-    } catch (const c10::Error &e) {
-        std::cout << e.backtrace() << std::endl;
+    } catch (const c10::Error& e) {
         std::cerr << "Error loading model\n";
     }
 
     std::cout << "Model loaded successfully\n";
 
-//    torch::NoGradGuard no_grad; // ensures that autograd is off
-//    module.eval(); // turn off dropout and other training-time layers/functions
+    torch::NoGradGuard no_grad; // ensures that autograd is off
+    module.eval(); // turn off dropout and other training-time layers/functions
 }
 
 bool Evaluator::evaluate(std::vector<uint8_t> a, std::vector<uint8_t> b) {
@@ -23,7 +21,7 @@ bool Evaluator::evaluate(std::vector<uint8_t> a, std::vector<uint8_t> b) {
 
     at::Tensor output = module.forward(inputs).toTensor();
 
-    if (torch::argmax(output).equal(torch::tensor(0))) {
+    if(torch::argmax(output).equal(torch::tensor(0))) {
         return true;
     } else {
         return false;
